ee07e8a1e2ded5643916f4de6499a6483ff2921e
aidenosi <aidenosi@yahoo.ca>
2019-06-13 16:24:40 -0400

    Continuing to work through YouTube tutorial and experimenting with different functionalities and features.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,18 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    id: this.props.id,
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = () => {
     this.setState({ value: this.state.value + 1 });
   };
 
+  handleClear = () => {
+    this.setState({ value: 0 });
+  };
+
   render() {
-    console.log(this.state.id);
     return (
       <div>
         {/* Render children of this object */}
@@ -22,6 +24,18 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.handleClear()}
+          className="btn-warning btn-sm m-2"
+        >
+          Clear
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,23 +2,53 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  i = 0;
   state = {
-    counters: [
-      /* array of counter objects */
-      { id: ++this.i, value: 0 },
-      { id: ++this.i, value: 0 },
-      { id: ++this.i, value: 0 },
-      { id: ++this.i, value: 0 }
-    ]
+    /* array of counter objects */
+    counters: [0, 0, 0, 0].map((value, i) => ({
+      id: i + 1,
+      value
+    }))
   };
+
+  handleDelete = counterId => {
+    console.log("Delete handler called on id:", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleAdd = () => {
+    let x = this.state.counters.length;
+    if (x !== 0) {
+      x = this.state.counters[x - 1].id;
+    }
+    console.log("Add handler called with id:", x + 1);
+    const counters = [...this.state.counters, { id: x + 1, value: 0 }];
+    this.setState({ counters });
+  };
+
+  handleClearAll = () => {};
 
   render() {
     return (
       <div>
+        <span>
+          <button onClick={this.handleAdd} className="btn-secondary btn-sm m-2">
+            Add Counter
+          </button>
+          <button
+            onClick={this.handleClearAll}
+            className="btn-warning btn-sm m-2"
+          >
+            Clear All Counters
+          </button>
+        </span>
         {/* Map each counter object to a Counter with key and value attributes*/}
         {this.state.counters.map(counter => (
-          <Counter id={counter.id} value={counter.value}>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          >
             {/*The <h4> is a Child of the Counter*/}
             <h4>Counter #{counter.id}</h4>
           </Counter>
